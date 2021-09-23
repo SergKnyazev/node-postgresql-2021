f4d5b5564c6d017c71ec7313761a0223a841291e
Serg <a7mda7s@mail.ru>
2021-09-23 12:53:23 +0300

    2021-09-23 branch sequelize-model part 0 : app.js

--- todos.js
+++ todos.js
@@ -1,7 +1,9 @@
 //TODO ::: 1) Sequelize --> SQL
 //TODO :::    branch = sequelize-sql
-//TODO ::: 2) Sequelize --> Model
-//TODO :::    branch = sequelize-model
+
+//TODO +++ 2) Sequelize --> Model
+//TODO +++    branch = sequelize-model
+
 //TODO ::: 3) supabase --> SQL
 //TODO :::    branch = supabase-sql
 //TODO ::: 4) supabase --> supabase API + subscribe on events
